75e8683bfa8b89ba75f620e6e6ea1d1b3617ce03
Amarjeet Singh Rai <jatt27@gmail.com>
2014-01-25 11:36:23 +0000

    Implement features.

--- Toggle Hidden Files/AppDelegate.m
+++ Toggle Hidden Files/AppDelegate.m
@@ -15,4 +15,43 @@
     // Insert code here to initialize your application
 }
 
+NSString *runCommand(NSString *commandToRun)
+{
+    NSTask *task;
+    task = [[NSTask alloc] init];
+    [task setLaunchPath: @"/bin/sh"];
+    
+    NSArray *arguments = [NSArray arrayWithObjects:
+                          @"-c" ,
+                          [NSString stringWithFormat:@"%@", commandToRun],
+                          nil];
+    NSLog(@"run command: %@",commandToRun);
+    [task setArguments: arguments];
+    
+    NSPipe *pipe;
+    pipe = [NSPipe pipe];
+    [task setStandardOutput: pipe];
+    
+    NSFileHandle *file;
+    file = [pipe fileHandleForReading];
+    
+    [task launch];
+    
+    NSData *data;
+    data = [file readDataToEndOfFile];
+    
+    NSString *output;
+    output = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+    return output;
+}
+
+- (IBAction)showHiddenFiles:(id)sender {
+    runCommand(@"defaults write com.apple.finder AppleShowAllFiles TRUE");
+    runCommand(@"killall Finder");
+}
+- (IBAction)hideHiddenFiles:(id)sender {
+    runCommand(@"defaults write com.apple.finder AppleShowAllFiles FALSE");
+    runCommand(@"killall Finder");
+}
+
 @end
